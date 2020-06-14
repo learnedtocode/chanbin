0d6d01457b3908337970cb70d8fe4069ed17d56d
learnedtocode <66190005+learnedtocode@users.noreply.github.com>
2020-06-14 01:45:45 +0000

    basic routing

--- public/index.php
+++ public/index.php
@@ -1,9 +1,26 @@
 <?php
 
-if (isset($_GET['showheaders'])) {
+require dirname(__DIR__) . '/config.php';
+
+$route = strtok($_SERVER['REQUEST_URI'], '?');
+
+$route_c = '/' . trim($route, '/');
+if ($route_c !== $route) {
+	$qs = ($_SERVER['QUERY_STRING'] ? '?' . $_SERVER['QUERY_STRING'] : '');
+	header('HTTP/1.1 302 Found');
+	header('Location: ' . $route_c . $qs);
+	die('Redirecting...');
+}
+
+if ($route === '/debug-' . $config['debug_password']) {
 	echo '<pre>';
 	foreach ($_SERVER as $name => $value) echo "$name: $value\n";
 	echo '</pre>';
-}
 
-require __DIR__ . '/coming-soon.php';
+} else if ($route === '/') {
+	require dirname(__DIR__) . '/pages/coming-soon.php';
+
+} else {
+	header('HTTP/1.1 404 Not Found');
+	die('<h2>Page Not Found</h2>');
+}
