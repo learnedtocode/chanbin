1298307800a4155764789c76c6eed67f01e30456
learnedtocode <66190005+learnedtocode@users.noreply.github.com>
2020-06-15 03:09:47 +0000

    about

--- pages/about.php
+++ pages/about.php
@@ -2,13 +2,8 @@
 
 page_header('About');
 
-?>
-<div id="page-text">
-	<h2>About this site</h2>
-	<p>This is a site like <a href="https://pastebin.com" rel="noopener noreferrer">pastebin.com</a> but this one is specifically for qresearch dough and handoffs.</p>
-	<p>If you don't know what that is, then you should use <a href="https://pastebin.com" rel="noopener noreferrer">pastebin.com</a> instead.</p>
-	<p><strong class="warning">Pastes submitted here that are not qresearch dough or handoffs may be deleted or rejected.</strong> You have been warned.</p>
-</div>
-<?php
+echo '<div id="page-text">';
+run_hooks('about');
+echo '</div>';
 
 page_footer();
